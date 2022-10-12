dc687c4688afb744d6e4eb62deabf0468dc1d3e5
Mike Szczys <szczys@hotmail.com>
2022-10-12 15:08:28 -0500

    Get webcam by-id

--- c920webcam/reset-zoom.sh
+++ c920webcam/reset-zoom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/v4l2-ctl -d /dev/video0 --set-ctrl=zoom_absolute=100
-/usr/bin/v4l2-ctl -d /dev/video0 --set-ctrl=exposure_auto=3
+/usr/bin/v4l2-ctl -d /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_83F655AF-video-index0 --set-ctrl=zoom_absolute=100
+/usr/bin/v4l2-ctl -d /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_83F655AF-video-index0 --set-ctrl=exposure_auto=3
