4c3ac8b92762f27c69ea796530ec57ea4ab26ba0
Mike Szczys <szczys@hotmail.com>
2023-11-11 23:44:55 -0600

    vim: telescope: open in normal mode by default
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -5,3 +5,9 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+
+require('telescope').setup{
+  defaults = {
+    initial_mode = "normal"
+  }
+}
