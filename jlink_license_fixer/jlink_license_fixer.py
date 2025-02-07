e5673ec2b5530d40fbe2f1e446b338c77db09f4e
Mike Szczys <szczys@hotmail.com>
2025-02-07 14:17:24 -0600

    jlink_license_fixer: update for new xml schema
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- jlink_license_fixer/jlink_license_fixer.py
+++ jlink_license_fixer/jlink_license_fixer.py
@@ -3,11 +3,16 @@ import os.path
 
 # <Registry>
 #   <Software>
-#     <SEGGER LogFileJLink="" LogFileJLink_SEGGERRegType="SYS_REG_TYPE_SZ" LogFileFunc="" LogFileFunc_SEGGERRegType="SYS_REG_TYPE_SZ">
-#       <J-Link StartMinimized="0x00000001" StartMinimized_SEGGERRegType="SYS_REG_TYPE_DWORD" StayOnTop="0x00000001" StayOnTop_SEGGERRegType="SYS_REG_TYPE_DWORD" LogIsHalted="0x00000001" LogIsHalted_SEGGERRegType="SYS_REG_TYPE_DWORD" LogInternal="0x00000001" LogInternal_SEGGERRegType="SYS_REG_TYPE_DWORD" OverrideConfigFile="0x00000000" OverrideConfigFile_SEGGERRegType="SYS_REG_TYPE_DWORD" OverrideLogFile="0x00000000" OverrideLogFile_SEGGERRegType="SYS_REG_TYPE_DWORD" ConfigFile="" ConfigFile_SEGGERRegType="SYS_REG_TYPE_SZ" LogFile="" LogFile_SEGGERRegType="SYS_REG_TYPE_SZ" LicenseLPCLink2_DontShowAgainToday="0x07E40A02" LicenseLPCLink2_DontShowAgainToday_SEGGERRegType="SYS_REG_TYPE_DWORD"/>
+#     <SEGGER LogFileJLink="" LogFileJLink_SEGGERRegType="SYS_REG_TYPE_SZ">
+#       <J-Link StartMinimized="0x00000001" StartMinimized_SEGGERRegType="SYS_REG_TYPE_DWORD" StayOnTop="0x00000001" StayOnTop_SEGGERRegType="SYS_REG_TYPE_DWORD" LogIsHalted="0x00000001" LogIsHalted_SEGGERRegType="SYS_REG_TYPE_DWORD" LogInternal="0x00000001" LogInternal_SEGGERRegType="SYS_REG_TYPE_DWORD" OverrideConfigFile="0x00000000" OverrideConfigFile_SEGGERRegType="SYS_REG_TYPE_DWORD" OverrideLogFile="0x00000000" OverrideLogFile_SEGGERRegType="SYS_REG_TYPE_DWORD" ConfigFile="" ConfigFile_SEGGERRegType="SYS_REG_TYPE_SZ" LogFile="" LogFile_SEGGERRegType="SYS_REG_TYPE_SZ" NotifyOnSecurityUpdate="0x00000000" NotifyOnSecurityUpdate_SEGGERRegType="SYS_REG_TYPE_DWORD" UpdateNotificationMode="0x00000000" UpdateNotificationMode_SEGGERRegType="SYS_REG_TYPE_DWORD">
+#         <DontShowAgain LicenseLPCLink2_DontShowAgainToday="0x07E90207" LicenseLPCLink2_DontShowAgainToday_SEGGERRegType="SYS_REG_TYPE_DWORD"/>
+#       </J-Link>
 #     </SEGGER>
 #   </Software>
 # </Registry>
+
+# Line where `*_DontShowAgainToday` is found
+TARGET_LINE = 4
 
 def patch_for_debugger(kind):
     splitter = f'License{kind}_DontShowAgainToday="'
@@ -15,8 +20,9 @@ def patch_for_debugger(kind):
     registry = os.path.expanduser("~/.config/SEGGER/SEGGER_REG_HKEY_CURRENT_USER.xml")
     lines = open(registry).readlines()
 
-    line3 = lines[3]
-    before, rest = line3.split(splitter)
+    target_line = lines[TARGET_LINE]
+    print(target_line)
+    before, rest = target_line.split(splitter)
     prev_date, after = rest.split('"', 1)
 
     today = date.today()
@@ -42,7 +48,7 @@ def patch_for_debugger(kind):
     new_date = f"0x{year}{month}{day}"
     print(f"bumping date from {prev_date} ({old_date}) -> {new_date} ({today})")
 
-    lines[3] = before + splitter + new_date + '"' + after
+    lines[TARGET_LINE] = before + splitter + new_date + '"' + after
 
     with open(registry, "w") as fh:
         for line in lines:
